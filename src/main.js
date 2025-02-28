b42933b8324074cd306eaf95845246d166df6576
a9521 <a952135763@hotmail.com>
2025-02-28 18:05:44 +0800

    待完成

--- src/main.js
+++ src/main.js
@@ -9,45 +9,49 @@ const renderer = new THREE.WebGLRenderer({alpha: true,antialias:true});
 const stats = new Stats();
 document.body.appendChild(stats.domElement);
 var myinfo = {
-  "init_velocity":1000,
-  "ballistic_coeff":-0.0001,
-  "position":new THREE.Vector3(0,0,0),
-  "velocity":new THREE.Vector3(0,0,0),
-  "direction":new THREE.Vector3(0,0,0),
+  "init_velocity":785,
+  "mass":0.092,
+  "caliber":0.02,
+  "length":0.5678,
+  "position":new THREE.Vector3(23734.2637,2431.20166,2608.37378),
+  "velocity":new THREE.Vector3(-7.18601704,-5.23217010,147.382675),
+  "direction":new THREE.Vector3(1,0,0),
 };
 var targetinfo ={
-  "position":new THREE.Vector3(0,0,0),
-  "velocity":new THREE.Vector3(0,0,0),
-  "acceleration":new THREE.Vector3(0,0,0),
+  "position":new THREE.Vector3(23457.7285,2370.69775,4528.65820),
+  "velocity":new THREE.Vector3(43.5342674,0.563053310,-132.302429),
+  "acceleration":new THREE.Vector3(-10.7967958,0.118002892,-5.90927219),
 };
 var mygui = gui.addFolder('MyInfo');
 mygui.add(myinfo, 'init_velocity' );
-mygui.add(myinfo, 'ballistic_coeff');
+mygui.add(myinfo, 'mass');
+mygui.add(myinfo, 'caliber');
+mygui.add(myinfo, 'length');
 var folder = mygui.addFolder('Position');
-folder.add(myinfo.position, 'x' );
-folder.add(myinfo.position, 'y' );
-folder.add(myinfo.position, 'z' );
+folder.add(myinfo.position, 'x' ).listen();
+folder.add(myinfo.position, 'y' ).listen();
+folder.add(myinfo.position, 'z' ).listen();
 folder = mygui.addFolder('velocity');
-folder.add(myinfo.velocity, 'x' );
-folder.add(myinfo.velocity, 'y' );
-folder.add(myinfo.velocity, 'z' );
+folder.add(myinfo.velocity, 'x' ).listen();
+folder.add(myinfo.velocity, 'y' ).listen();
+folder.add(myinfo.velocity, 'z' ).listen();
 folder = mygui.addFolder('direction');
-folder.add(myinfo.direction, 'x' );
-folder.add(myinfo.direction, 'y' );
-folder.add(myinfo.direction, 'z' );
+folder.add(myinfo.direction, 'x' ).listen();
+folder.add(myinfo.direction, 'y' ).listen();
+folder.add(myinfo.direction, 'z' ).listen();
 var targetgui = gui.addFolder('TargetInfo');
 folder = targetgui.addFolder('Position');
-folder.add(targetinfo.position, 'x' );
-folder.add(targetinfo.position, 'y' );
-folder.add(targetinfo.position, 'z' );
+folder.add(targetinfo.position, 'x' ).listen();
+folder.add(targetinfo.position, 'y' ).listen();
+folder.add(targetinfo.position, 'z' ).listen();
 folder = targetgui.addFolder('velocity');
-folder.add(targetinfo.velocity, 'x' );
-folder.add(targetinfo.velocity, 'y' );
-folder.add(targetinfo.velocity, 'z' );
+folder.add(targetinfo.velocity, 'x' ).listen();
+folder.add(targetinfo.velocity, 'y' ).listen();
+folder.add(targetinfo.velocity, 'z' ).listen();
 folder = targetgui.addFolder('acceleration');
-folder.add(targetinfo.acceleration, 'x' );
-folder.add(targetinfo.acceleration, 'y' );
-folder.add(targetinfo.acceleration, 'z' );
+folder.add(targetinfo.acceleration, 'x' ).listen();
+folder.add(targetinfo.acceleration, 'y' ).listen();
+folder.add(targetinfo.acceleration, 'z' ).listen();
 //监听按钮+键保存信息 相机信息和目标信息
 window.addEventListener('keydown', (event) => {
  if(event.key == '+'){
@@ -87,26 +91,27 @@ var Bullet = function(start_position,s_velocity,init_velocity,direction,ballisti
   this.clock = new THREE.Clock()
 
   this.mesh = new THREE.Object3D();
-  var geometry = new THREE.SphereGeometry(5, 32, 32);
+  var geometry = new THREE.SphereGeometry(1, 32, 32);
   var material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
   var bullet = new THREE.Mesh(geometry, material);
   this.mesh.add(bullet);
   this.init_position = start_position;
   this.dist = 0;
   this.mesh.position.copy(start_position);
-  
+
+  //计算弹丸3维速度 s_velocity 是载具速度 direction 是方向 init_velocity 是初速度
   this.velocity = s_velocity.clone().add(direction.clone().multiplyScalar(init_velocity));
   this.ballistic_coeff = ballistic_coeff;
   this.end = false;
-  scene.add(this.mesh);
+  
   //this.last_time = new Date().getTime();
   this.arrow = [];
   this.count = 1;
-  
+  this.update_end = false;
+  this.time = 0;
 
   this.UpdateProjectile = (time)=>{
-    //console.log(time);
-    //记录弹丸轨迹点
+
     var vel = this.velocity;
     var velocityLengthSqr = vel.lengthSq();
     var ballisticTerm = Math.sqrt(velocityLengthSqr) * time * this.ballistic_coeff;
@@ -118,14 +123,9 @@ var Bullet = function(start_position,s_velocity,init_velocity,direction,ballisti
     this.velocity.y = this.velocity.y * adjustmentFactor + (-9.81*time);
     this.mesh.position.add(this.velocity.clone().multiplyScalar(time));
     this.dist = this.mesh.position.distanceTo(this.init_position);
-
-
     if(++this.count >10){
       this.count = 0;
-
-      //拟合成曲线
       var dir = this.velocity.clone().normalize();
-      
       var arrowe = new THREE.ArrowHelper(
           dir,
           this.mesh.position.clone(),
@@ -138,6 +138,7 @@ var Bullet = function(start_position,s_velocity,init_velocity,direction,ballisti
   };
 
   this.update = ()=>{
+    var ctime = 1/96;
     if(this.end){
       //绘制记录点
       
@@ -147,11 +148,15 @@ var Bullet = function(start_position,s_velocity,init_velocity,direction,ballisti
     if(this.mesh.position.y < 0 || this.dist > 10000){
       this.end = true;
     }
-    this.UpdateProjectile(1/96);
+    this.UpdateProjectile(ctime);
+    //判断this.update_end是否是函数 是就调用
+    if(typeof this.update_end == 'function'){
+      this.update_end(ctime);  
+    }
+    this.time += ctime;
 
   };
   this.remove = ()=>{
-    scene.remove(this.mesh);
     for(var i = 0; i < this.arrow.length; i++){
       scene.remove(this.arrow[i]);
     }
@@ -203,19 +208,27 @@ var AirPlane = function() {
   this.velocity = new THREE.Vector3();//飞机速度
   this.acceleration = new THREE.Vector3();//飞机加速度
 
-  //根据速度和加速度推算飞机的下一个位置
   this.update = ()=>{
     this.propeller.rotation.x += 0.1;
-    //var out_pos = new THREE.Vector3();
-    //var out_vel = new THREE.Vector3();
-    //todo::尝试画出圆心轨迹
-    //extrapolateCircular(this.mesh.position,this.velocity,this.acceleration,1/96,out_pos,out_vel);
-    //this.mesh.position.copy(out_pos);
-    //this.velocity.copy(out_vel);
   };
   this.remove = ()=>{
-    //scene.remove(this.mesh);
-  }
+  };
+  this.predicted_data = [];
+  //预测位置 记录预测信息
+  this.predicted = (time)=>{
+    var out_pos = new THREE.Vector3();
+    var out_vel = new THREE.Vector3();
+    //todo::尝试画出圆心轨迹
+    extrapolateCircular(this.mesh.position,this.velocity,this.acceleration,time,out_pos,out_vel);
+    //记录预测的信息
+    var data = {
+      "position":out_pos,
+      "velocity":out_vel,
+    };
+    //this.predicted_data.push(data);
+    return data;
+  };
+
 };
 //新建一个弹丸管理器,管理并更新所有弹丸
 var BulletManager = function(){
@@ -228,7 +241,9 @@ var BulletManager = function(){
   //创建弹丸
   this.CreateBullet = (start_position,velocity,init_velocity,direction,ballistic_coeff)=>{
     var bullet = new Bullet(start_position,velocity,init_velocity,direction,ballistic_coeff);
-    this.bullets.push(bullet);  
+    this.bullets.push(bullet);
+    scene.add(bullet.mesh);
+    return bullet;
   }
   //删除最先创建的弹丸
   this.DeleteBullet = ()=>{
@@ -236,6 +251,7 @@ var BulletManager = function(){
     var buller = this.bullets.shift();
     if(buller){
       buller.remove();
+      scene.remove(buller.mesh);
     } 
   }
   //监听键盘事件小键盘1键删除一个弹丸 2键创建一个弹丸
@@ -246,11 +262,9 @@ var BulletManager = function(){
     if(e.key === "2"){
       //创建弹丸 需要一些参数 如何让用户输入?
       //打印myinfo
-      console.log(myinfo);
-      this.CreateBullet(myinfo.position,myinfo.velocity,myinfo.init_velocity,myinfo.direction,myinfo.ballistic_coeff);
-    }
-    if(e.key === "3"){
-      //可能击中目标的方式
+      var coff = getAirDensity(myinfo.position.y);
+      coff = getAirResistanceCoefficient(coff,myinfo.mass,myinfo.caliber,myinfo.length);
+      this.CreateBullet(myinfo.position,myinfo.velocity,myinfo.init_velocity,myinfo.direction,coff);
     }
   });
 };
@@ -291,7 +305,7 @@ var airplaneManager = new AirPlaneManager();
 function createLights() {
   var hemisphereLight = new THREE.HemisphereLight(0xaaaaaa,0x000000, .9)
   var shadowLight = new THREE.DirectionalLight(0xffffff, .9);
-  shadowLight.position.set(999, 999, 999);
+  shadowLight.position.set(9999, 9999, 9999);
 
   scene.add(hemisphereLight);  
   scene.add(shadowLight);
@@ -319,8 +333,22 @@ const cube = new THREE.Mesh(geometry, material);
 
 
 //创建物体
-var target = airplaneManager.CreateAirPlane(new THREE.Vector3(1000,136,1000),new THREE.Vector3(0,0,0),new THREE.Vector3(0,0,0));
+var my = airplaneManager.CreateAirPlane(myinfo.position,myinfo.velocity,new THREE.Vector3());
 
+var target = airplaneManager.CreateAirPlane(targetinfo.position,targetinfo.velocity,targetinfo.acceleration);
+target.mesh.scale.set(.25,.25,.25);
+my.mesh.scale.set(.25,.25,.25);
+
+gui.onFinishChange(()=>{
+  //配置飞机信息
+  target.mesh.position.copy(targetinfo.position);
+  target.velocity.copy(targetinfo.velocity);
+  target.acceleration.copy(targetinfo.acceleration);
+
+  my.mesh.position.copy(myinfo.position);
+  my.velocity.copy(myinfo.velocity);
+ 
+});
 
 
 scene.add(cube);
@@ -343,6 +371,92 @@ function animate() {
   renderer.render(scene, camera);
 }
 animate();
+
+//根据 高度获取空气阻力系数
+function getAirDensity(height) {
+  var density = 1.225; // 标准大气压下的空气密度
+  //最高海拔 18300.0
+  var height_clamped = Math.min(height,18300.0);
+  var polynomial = height_clamped * 2.2872e-19;
+  polynomial+= -5.8356e-14;
+  polynomial*= height_clamped;
+  polynomial += 0.0000000035312;
+  polynomial *= height_clamped;
+  polynomial += -0.000095939;
+  polynomial *= height_clamped;
+  var base_value = 18300.0 * 1.225;
+  var density = base_value + (polynomial * base_value);
+  density /= Math.max(18300.0, height);
+  return density;
+}
+//根据弹丸重量 口径 长度 计算弹丸空气阻力系数
+function getAirResistanceCoefficient(density, mass, caliber, length) {
+  // 计算弹丸的横截面积
+  const crossSectionalArea = Math.pow(caliber * 0.5, 2) * Math.PI;
+  
+  // 计算空气阻力系数的中间值
+  const intermediateValue = crossSectionalArea * length * density * 0.5;
+  
+  // 计算最终的空气阻力系数
+  const airResistanceCoefficient = intermediateValue / mass;
+  
+  return airResistanceCoefficient * -1;
+}
+
+
+window.addEventListener("keydown",(e)=>{
+  if(e.key === " "){
+    var coff = getAirDensity(myinfo.position.y);
+    coff = getAirResistanceCoefficient(coff,myinfo.mass,myinfo.caliber,myinfo.length);
+
+
+
+    //计算目标相对位置
+    var relative_pos = target.mesh.position.clone().sub(my.mesh.position);
+    var relative_magnitude = relative_pos.clone().length();//相对距离
+    var relative_drirection = relative_pos.clone().normalize();
+
+    
+
+
+    //创建弹丸
+    var bull = bulletManager.CreateBullet(my.mesh.position,my.velocity,myinfo.init_velocity,relative_drirection,coff);
+    var time = 0;
+    bull.update_end = (ctime)=>{
+      if(bull.end){
+        return;
+      }
+
+
+      time += ctime;
+      var newpos = target.predicted(time);
+      //计算弹丸到我的位置的距离
+      var bull_distance = bull.mesh.position.clone().sub(my.mesh.position).lengthSq();
+      //计算目标到我的位置的距离
+      var target_distance = newpos.position.clone().sub(my.mesh.position).lengthSq();
+      //如果弹丸到我的位置的距离大于目标到我的位置的距离 插值计算他们距离到我的距离相等的位置
+      if(bull_distance > target_distance){
+        bull.end = true;
+        //计算弹丸到目标的距离
+        var bull2newpos = bull.mesh.position.clone().sub(newpos.position.clone()).length();
+        //计算插值系数
+        var ins = (Math.sqrt(bull_distance) - Math.sqrt(target_distance)) / bull2newpos;
+        //保证插值系数在0-1之间
+        ins = Math.max(0,Math.min(1,ins));
+        //插值计算弹丸到目标的位置
+        var newpos2 = newpos.position.clone().multiplyScalar(ins).add(bull.mesh.position.clone().multiplyScalar(1-ins));
+        //插值时间
+        time = time + ctime - (ins * ctime);
+        //外推目标最新位置
+        var newpos3 = target.predicted(time);
+
+        
+      }
+
+
+    }
+  }
+})
 
 
 /**
